c93ca205bff9289028e5d91a66ce2489b1effde0
Nikola Bursac <nikola.bursac@proton.me>
2023-12-26 16:26:36 +0100

    Add Bull queues for notifying employees

---
+++ src/employees/notify/notify.module.ts
@@ -0,0 +1,15 @@
+import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
+import { NotifyProcessor } from './notify.processor';
+@Module({
+    imports: [
+        BullModule.registerQueue({
+            name: 'notify',
+            defaultJobOptions: {
+                removeOnComplete: true,
+            },
+        }),
+    ],
+    providers: [NotifyProcessor],
+})
+export class NotifyModule {}
---
+++ src/employees/notify/notify.processor.ts
@@ -0,0 +1,59 @@
+import { Process, Processor } from '@nestjs/bull';
+import { Job } from 'bull';
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+
+@Injectable()
+@Processor('notify')
+export class NotifyProcessor {
+    constructor(private readonly mailer: MailerService) {}
+
+    @Process('employee:created')
+    created(job: Job) {
+        this.mailer.sendMail({
+            to: job.data?.employee?.email,
+            from: 'noreply@mydomain.com',
+            subject: 'Your account details for ACME corp!',
+            template: 'employee.created.html.hbs',
+            context: {
+                company: 'ACME Company',
+                email: job.data?.employee?.email,
+                first_name: job.data?.employee?.first_name,
+                last_name: job.data?.employee?.last_name,
+                password: job.data?.password?.password,
+            },
+        });
+    }
+
+    @Process('employee:updated')
+    updated(job: Job) {
+        this.mailer.sendMail({
+            to: job.data?.employee?.email,
+            from: 'noreply@mydomain.com',
+            subject: 'Your account UPDATED',
+            template: 'employee.updated.html.hbs',
+            context: {
+                company: 'ACME Company',
+                email: job.data?.employee?.email,
+                first_name: job.data?.employee?.first_name,
+                last_name: job.data?.employee?.last_name,
+            },
+        });
+    }
+
+    @Process('employee:deleted')
+    deleted(job: Job) {
+        this.mailer.sendMail({
+            to: job.data?.employee?.email,
+            from: 'noreply@mydomain.com',
+            subject: 'Your account DELETED',
+            template: 'employee.deleted.html.hbs',
+            context: {
+                company: 'ACME Company',
+                email: job.data?.employee?.email,
+                first_name: job.data?.employee?.first_name,
+                last_name: job.data?.employee?.last_name,
+            },
+        });
+    }
+}
